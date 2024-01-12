238a145acb57438d2ec930c320a1fd842bc6d771
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-01-12 17:02:57 +0600

    watch video page design updated

--- src/views/WatchVideo.vue
+++ src/views/WatchVideo.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="max-w-[1240px] mx-auto my-10">
-    <div class="h-[400px] w-[450px] object-cover mx-auto">
-      <video autoplay controls ref="videoRef" :src="selectedVideo?.src" ></video>
-    </div>
+  <div class="max-w-[1240px] mx-auto p-8 md:p-12">
+    <div class="max-h-[600px] max-w-[650px] object-cover mx-auto rounded-[20px] border-[20px] border-[#2a8c930d] shadow-[rgba(50,50,93,0.25)_0px_2px_5px_-1px,rgba(0,0,0,0.3)_0px_1px_3px_-1px]">
+        <video autoplay controls ref="videoRef" :src="selectedVideo?.src" ></video>
+      </div>
   </div>
 </template>
 
